510ed4f7748ec57a984d0dfe2905d7e19eb5c1d1
SamiOMGgurl <83353299+SamiOMGgurl@users.noreply.github.com>
2022-07-04 17:26:05 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.canvas("minecraftcanvas");
+
+block_image_width = 50;
+block_image_height = 50;
+
+player_x = 10;
+player_y = 10;
+
+var player_object= "";
+
+function player_update()
+{ 
+    fabric.Image.fromURL("GAFI.png", function(Img)
+    (
+    { player_object = Img;
+    
+    player_object.scaleToWidth(150);
+    player_object.scaleToHeight(140);
+    player_object.set({
+        top:player_y,
+        left:player_x
+    });
+    canvas.add(player_object);
+});
+
+}
+
+
+
+function new_image(get_image)
+{ 
+    fabric.Image.fromURL(get_image, function(Img) 
+    {
+
+    (
+    { block_image_object = Img;
+    
+   block_image_object.scaleToWidth(block_image_width);
+   block_image_object.scaleToHeight(block_image_height);
+   block_image_object.set({
+        top:player_y,
+        left:player_x
+    });
+    canvas.add(block_image_object);
+});
+
+}
+
